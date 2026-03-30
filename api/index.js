5e4c7e00f7db2d8439d844bd503dc9332c6d1d61
elvinjack559-svg <elvinjack559@gmail.com>
2026-03-30 23:10:22 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
-export const config = {
-  runtime: 'edge',
-};
-
+export const config = { runtime: 'edge' }; // 强制开启边缘模式，不限时
 export default async function handler(req) {
   const url = new URL(req.url);
   url.host = "generativelanguage.googleapis.com";
